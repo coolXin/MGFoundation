45a93f10c2ae54410d24d6faf6c8d36b5ef6a378
陈欣 <380196472@qq.com>
2018-06-07 11:51:09 +0800

    add# 添加测试文件

---
+++ MGFoundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGFoundation
+//
+//  Created by chenxin on 2018/6/7.
+//  Copyright © 2018年 chenxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MGFoundation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGFoundation
+//
+//  Created by chenxin on 2018/6/7.
+//  Copyright © 2018年 chenxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
